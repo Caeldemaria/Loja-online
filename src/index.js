e13c5f005b7d279b05d3cf3ecedea7e3f747cb19
nathasha <thashadeoliveira@gmail.com>
2023-12-17 00:23:00 -0300

    continuar

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ const app= express()
 app.use(express.json());
 
 app.get('/teste',async (req,res)=>{
-    const listaAleatoria = [
-        'carro',
-        'casa',
-        'forno',
-        'guardaRoupa'
-    ]
-    res.status(202).json(listaAleatoria)
+   
 })
 const port= process.env.PORT || 3000
 app.listen(port,()=>{
-    console.log(`api funcionando perfeitamente na porta ${port}`)
+    console.log(`api funcionando perfeitamente na porta ${port}`);
 })
---
+++ src/usuarios.js
@@ -0,0 +1,2 @@
+const 
+const bcrypt=require('bcrypt')
